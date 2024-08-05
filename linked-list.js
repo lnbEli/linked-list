76e873889fc7c42d59afe9dbeb45210bf5005598
lnbEli <137424458+lnbEli@users.noreply.github.com>
2024-08-05 18:06:51 +0200

    Add size, head, tail and indexAt methods

--- linked-list.js
+++ linked-list.js
@@ -6,41 +6,78 @@ class Node {
     this.nextNode = nextNode;
   }
 }
-function returnPenultimateNode(node, lastNode) {
-  if (node.nextNode === lastNode) {
+function returnPenultimateNode(node) {
+  if (node.nextNode.nextNode === null) {
     return node;
-  } else return returnPenultimateNode(node.nextNode, lastNode);
+  } else return returnPenultimateNode(node.nextNode);
+}
+
+function returnLinkedListLength(node, count = 0) {
+  if (!node) {
+    return count;
+  } else {
+    count++;
+    return returnLinkedListLength(node.nextNode, count);
+  }
+}
+
+function nodeAtIndex(node, index, count = 0) {
+  if (node === null) {
+    return null;
+  } else if (index === count) {
+    return node;
+  } else {
+    count++;
+    return nodeAtIndex(node.nextNode, index, count);
+  }
 }
 
 class LinkedList {
-  head = null;
-  tail = null;
+  headNode = null;
+  tailNode = null;
 
   append(value) {
-    if (!this.head) {
-      this.head = new Node(value, this.tail);
-    } else if (this.head && !this.tail) {
-      this.tail = new Node(value);
-      this.head.nextNode = this.tail;
+    if (!this.headNode) {
+      this.headNode = new Node(value, this.tailNode);
+    } else if (this.headNode && !this.tailNode) {
+      this.tailNode = new Node(value);
+      this.headNode.nextNode = this.tailNode;
     } else {
-      const node = new Node(this.tail.value, this.tail);
-      const penultimateNode = returnPenultimateNode(this.head, this.tail);
-      this.tail.value = value;
+      const node = new Node(this.tailNode.value, this.tailNode);
+      const penultimateNode = returnPenultimateNode(this.headNode);
       penultimateNode.nextNode = node;
+      this.tailNode.value = value;
     }
   }
 
   prepend(value) {
-    if (!this.head) {
-      this.head = new Node(value, this.tail);
-    } else if (this.head && !this.tail) {
-      this.tail = new Node(this.head.value);
-      this.head.value = value;
+    if (!this.headNode) {
+      this.headNode = new Node(value, this.tailNode);
+    } else if (this.headNode && !this.tailNode) {
+      this.tailNode = new Node(this.headNode.value);
+      this.headNode.value = value;
+      this.headNode.nextNode = this.tailNode;
     } else {
-      const node = new Node(this.head.value, this.head.nextNode);
-      this.head.value = value;
-      this.head.nextNode = node;
+      const node = new Node(this.headNode.value, this.headNode.nextNode);
+      this.headNode.value = value;
+      this.headNode.nextNode = node;
     }
+  }
+
+  size() {
+    return returnLinkedListLength(this.headNode);
+  }
+
+  head() {
+    return this.headNode;
+  }
+
+  tail() {
+    return this.tailNode;
+  }
+
+  at(index) {
+    return nodeAtIndex(this.headNode, index);
   }
 }
 
@@ -52,7 +89,9 @@ list.append(80);
 list.prepend("hello");
 list.prepend("hi");
 
-console.log(inspect(list, { showHidden: false, depth: null, colors: true }));
+// console.log(inspect(list, { showHidden: false, depth: null, colors: true }));
+
+console.log(list.at(8));
 
 //1.append(value) adds a new node containing value to the end of the list
 //2.prepend(value) adds a new node containing value to the start of the list
